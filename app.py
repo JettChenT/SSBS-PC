d8882d688da27efe45cfd20448572cfe9bf3c191
Jettchen <jettchen12345@gmail.com>
2019-04-06 15:32:19 +0800

    Add cookie

--- app.py
+++ app.py
@@ -1,43 +1,29 @@
-from flask import Flask, render_template,redirect,request,session
+from flask import Flask, render_template,redirect,request,make_response
 import json
 # ----app init-----
 with open('votes.json','r') as f:
    data = json.load(f)
 app = Flask(__name__)
 class Candidate():
-   def __init__(self,name,position,story):
+   def __init__(self,name,story):
       self.name = name
       self.route =  '/'+name
-      self.position = position
       self.story = story
       self.votes = data[self.name]
-      # Vote
-      # session[name] = 0
    def vote(self):
       data[self.name]+=1
       with open('votes.json','w') as f:
          json.dump(data,f)
-Thanos = Candidate('Thanos','president','Thanos wiped out half of the universe!!!')
-Loki = Candidate('Loki','vice president',"Loki isn't low-key!")
-Hela = Candidate('Hela','president',"Hela destroyed Thor's hammer!")
-Venom = Candidate('Venom','vice president','Venom eats your head!')
-Magneto = Candidate('Magneto','president','Magneto!!!')
-RedSkull = Candidate('Red Skull','vice president','Reeeeeeed Skulllllllllll!!!!!!')
-Ultron = Candidate('Ultron','president','Ultron is anywhere!')
-Surtur = Candidate('Surtur','vice president','Sutur is going to destroy Asguard!!!')
-candidatelist = [Thanos,Loki,Hela,Venom,Magneto,RedSkull,Ultron,Surtur]
-candidatenamelist = []
-plist = []
-vplist = []
-for villian in candidatelist:
-   candidatenamelist.append(villian.name)
-   if villian.position == 'president':
-      plist.append(villian.name)
-   else:
-      vplist.append(villian.name)
+TL = Candidate('Thanos and Loki','Thanos and Loki, the most powerful couple!')
+HV = Candidate('Hela and Venom',"Hela destroyed Thor's hammer, and Venom got Eminem's help!")
+MR = Candidate('Magneto and Red Skull','A cute cuple from WWII!')
+US = Candidate('Ultron and Surtur','AI and a giant!')
 
+candidatelist = [TL,HV,MR,US]
+candidatenamelist = []
+for i in candidatelist:
+   candidatenamelist.append(i.name) 
 # Routes
- 
 @app.route('/')
 def homepage():
    return render_template('index.html')
@@ -50,16 +36,10 @@ def generate(candidate):
    else:
       i = candidatenamelist.index(candidate)
       cdd = candidatelist[i]
-      lis = cdd.position.split(' ')
-      pst = ''.join(lis)
-      return render_template('candidate.html',candidate = cdd.name, position = cdd.position, story = cdd.story,pst = pst)
-
-@app.route('/vote/president')
-def voteforp():
-   return render_template('vote.html',position = 'President', candidatenamelist = plist)
-@app.route('/vote/vicepresident')
-def voteforvp():
-   return render_template('vote.html',position = 'Vice president', candidatenamelist = vplist)
+      return render_template('candidate.html',candidate = cdd.name, story = cdd.story)
+@app.route('/vote')
+def voting():
+   return render_template('vote.html',candidatenamelist = candidatenamelist)
 @app.route('/votesuccessful',methods = ['POST'])
 def check():
    candidatelist[candidatenamelist.index(request.form['p'])].vote()
